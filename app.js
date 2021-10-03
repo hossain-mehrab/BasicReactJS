a5d01c48167876f255803909b429d07e02e9176d
Md Mehrab Hossain <mhossain133066@bseee.uiu.ac.bd>
2021-10-03 22:11:43 +0300

    init first practice

---
+++ app.js
@@ -0,0 +1,29 @@
+ //ES6
+ //1. For ES6 use let and const (But in JS use var)
+  //. For Js
+  var myname = "Mehrab";
+  myname = "Hossain";
+  console.log(myname);
+ 
+  //. For ES6
+  let myname1 = "Mehrab";
+  myname1 = "Hossain";
+  console.log(myname1);
+
+  const myname2 = "Hasan";
+  console.log(myname2);
+
+ //2. For Arrow Function
+  //1. Normal JS function
+   function sayName(){
+       console.log("Mehrab32")
+   }
+   sayName();
+  //1. Normal JS function
+  function sayName(){
+    console.log("Mehrab32")
+  }
+   sayName();
+
+
+
